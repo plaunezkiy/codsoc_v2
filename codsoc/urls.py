d651da8b3ca9431417ad16fdf4a2817cd3213518
Nik <plush337@gmail.com>
2020-09-11 18:12:59 +0100

    500 fix update

--- codsoc/urls.py
+++ codsoc/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import handler404, handler500 # noqa
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-# handler404 = "modules.views.page_not_found" # noqa
-# handler500 = "modules.views.server_error" # noqa
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-admin-url/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('modules.urls'))
--- modules/views.py
+++ modules/views.py
@@ -17,3 +17,11 @@ def modules_app(request):
 class ModuleView(ModelViewSet):
     queryset = Module.objects.all()
     serializer_class = ModuleSerializer
+
+
+def page_not_found(request, exception):
+    return render(request, "misc/404.html", {"path": request.path}, status=404)
+
+
+def server_error(request):
+    return render(request, "misc/500.html", status=500)
